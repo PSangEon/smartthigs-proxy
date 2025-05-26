1bdd35f61e13f53ac50417d7ac30c0fcbd26e6b3
PSangEon <qkr99102@naver.com>
2025-05-26 23:13:40 +0900

    fix: add redirect url

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 // Token renewal logic (with correct curl-style request)
 async function getValidAccessToken() {
